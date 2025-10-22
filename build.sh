b2bf24700c64c617e306c09430671a48a84e3dee
ramesh-bobbadi <bobbadiramesh6@gmail.com>
2025-10-22 18:02:07 +0530

    create super user

--- build.sh
+++ build.sh
@@ -6,7 +6,8 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [[$CREATE_SUPERUSER]].;
-then
-python manage.py createsuperuser --no-input
+if [ "$CREATE_SUPERUSER" = "true" ]; then
+  python manage.py createsuperuser --no-input \
+    --username "$DJANGO_SUPERUSER_USERNAME" \
+    --email "$DJANGO_SUPERUSER_EMAIL"
 fi
