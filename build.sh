824d9772107f4034766ec191e2dc93ef1a279852
ramesh-bobbadi <bobbadiramesh6@gmail.com>
2025-10-22 18:13:54 +0530

    buidl.sh modfied

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ python manage.py collectstatic --no-input
 
 python manage.py migrate
 
-if [ "$CREATE_SUPERUSER" = "true" ]; then
+if [ "$CREATE_SUPERUSER" = "True" ]; then
   python manage.py createsuperuser --no-input \
     --username "$DJANGO_SUPERUSER_USERNAME" \
     --email "$DJANGO_SUPERUSER_EMAIL"
